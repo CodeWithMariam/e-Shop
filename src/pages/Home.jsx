9e16d7d8748b83fbe33024ff7625d481c24ce840
Mariam Kareem <mariamkareem772@gmail.com>
2024-12-23 23:28:00 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
   return (
     <div className='bg-white px-4'>
     <div className='bg-white mt-2 px-4 md:px-16 lg:px-24'>
-      <div className='container mx-auto py-4 flex flex-col md:flex-row space-x-2'>
+      <div className='container mx-auto py-4 flex flex-col md:flex-row md:space-x-2'>
        <div className='w-full md:w-3/12'>
               <div className='bg-red-600 text-white text-xs font-bold px-2 py-2.5'>SHOP BY CATEGORIES</div>
               <ul className='space-y-4 bg-gray-100 p-3 border'>
