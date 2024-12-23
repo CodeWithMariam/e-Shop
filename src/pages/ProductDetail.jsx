f81e50e333b12ffb48ee18dd7fb715a4e8138f35
Mariam Kareem <mariamkareem772@gmail.com>
2024-12-24 00:02:18 +0500

    Update ProductDetail.jsx

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -26,7 +26,7 @@ const ProductDetail = () => {
 
   return (
     <div className="container mx-auto px-4 md:px-16 lg:px-24 py-8">
-    <div className="flex flex-col md:flex-row space-x-4">
+    <div className="flex flex-col md:flex-row md:space-x-4">
         <div className="md:w-1/2 py-4 shadow-md md:px-8 flex justify-center">
             <img
                 src={product.image}
